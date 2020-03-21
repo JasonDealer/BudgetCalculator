28e8ce8675fdb6ea6082c6ab3235a3600808115a
Maksim <makcxx20@gmail.com>
2020-03-21 15:17:02 +0300

    expenses

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let array = ["ya", "ne", "gay"];
+let money = +prompt("Ваш бюджет на месяц?");
 
-console.log(array[1]);
+let time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let answer = confirm("U gay?");
+let qExpFirst = prompt("Введите обязательную статью расходов в этом месяце");
 
-console.log(answer);
+let qoExpFirst = +prompt("Во сколько обойдется?");
+
+let qExpSecond = prompt("Введите обязательную статью расходов в этом месяце");
+
+let qoExpSecond = +prompt("Во сколько обойдется?");
+
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses:{},
+    optionalEspenses:{},
+    income:[],
+    savings: false,
+};
+
+appData.expenses[qExpFirst] = qoExpFirst;
+appData.expenses[qExpSecond] = qoExpSecond;
+
+alert(("money" - "qoExpFirst" - "qoExpSecond")/30);
+
+
+console.log(appData);
